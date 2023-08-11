21fbf5e4f4171fa82b07b247148522f024e67bd3
Akash Pandey <112191907+akash-infocom@users.noreply.github.com>
2023-08-11 05:43:50 +0530

    Add files via upload

---
+++ Storedata.js
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+
+/*import { View, Text, TextInput } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState } from 'react';
+import { StyleSheet } from 'react-native';
+const Storedata = () => {
+
+    const [data, setData] = useState('');
+
+    const SaveData = async () => {
+        try {
+            await AsyncStorage.setItem('Data', data);
+            console.log('saved')
+        }
+        catch { error }
+    }
+};
+
+return (
+    <View>
+        <TextInput
+            value={data}
+            onChangeText={txt => setData(txt)}
+        />
+    </View>
+)
+}
+
+export default Storedata*/
+
